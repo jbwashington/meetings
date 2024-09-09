3b1f7b03e5f9d10ea891a83be861989fc0b75b32
James Washington <jbwashington@gmail.com>
2024-09-09 00:37:56 -0400

    started adding file upload to whisper

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { RecordButton } from "@/components/layout/record-button";
+import { AudioRecorder } from 'react-audio-voice-recorder';
+import { Messages } from "@/components/layout/messages";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <RecordButton />
+        <Messages />
         </main>
     );
 }

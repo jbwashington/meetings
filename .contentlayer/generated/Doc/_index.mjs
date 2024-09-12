49526530cc703ad0ede1ffbdf8b09e9ed8467c6d
James Washington <jbwashington@gmail.com>
2024-09-11 23:53:50 -0400

    added docs from operations manual and formatted

--- .contentlayer/generated/Doc/_index.mjs
+++ .contentlayer/generated/Doc/_index.mjs
@@ -2,10 +2,28 @@
 
 import docs__inProgressMdx from './docs__in-progress.mdx.json' with { type: 'json' }
 import docs__indexMdx from './docs__index.mdx.json' with { type: 'json' }
-import docs__documentation__codeBlocksMdx from './docs__documentation__code-blocks.mdx.json' with { type: 'json' }
-import docs__documentation__componentsMdx from './docs__documentation__components.mdx.json' with { type: 'json' }
+import docs__about__committeesMdx from './docs__about__committees.mdx.json' with { type: 'json' }
+import docs__about__electionsMdx from './docs__about__elections.mdx.json' with { type: 'json' }
+import docs__about__executiveBoardMdx from './docs__about__executive-board.mdx.json' with { type: 'json' }
+import docs__about__faqsMdx from './docs__about__faqs.mdx.json' with { type: 'json' }
+import docs__about__indexMdx from './docs__about__index.mdx.json' with { type: 'json' }
+import docs__about__missionMdx from './docs__about__mission.mdx.json' with { type: 'json' }
+import docs__documentation__bylawsMdx from './docs__documentation__bylaws.mdx.json' with { type: 'json' }
+import docs__documentation__externalResourcesMdx from './docs__documentation__external-resources.mdx.json' with { type: 'json' }
 import docs__documentation__indexMdx from './docs__documentation__index.mdx.json' with { type: 'json' }
 import docs__documentation__styleGuideMdx from './docs__documentation__style-guide.mdx.json' with { type: 'json' }
-import docs__about__indexMdx from './docs__about__index.mdx.json' with { type: 'json' }
+import docs__documentation__tnsPtaOperationalManualMdx from './docs__documentation__tns-pta-operational-manual.mdx.json' with { type: 'json' }
+import docs__about__meetings__indexMdx from './docs__about__meetings__index.mdx.json' with { type: 'json' }
+import docs__about__meetings__minutes__2022_23Mdx from './docs__about__meetings__minutes__2022-23.mdx.json' with { type: 'json' }
+import docs__about__meetings__minutes__2023_24Mdx from './docs__about__meetings__minutes__2023-24.mdx.json' with { type: 'json' }
+import docs__about__meetings__minutes__2024_25Mdx from './docs__about__meetings__minutes__2024-25.mdx.json' with { type: 'json' }
+import docs__about__meetings__minutes__indexMdx from './docs__about__meetings__minutes__index.mdx.json' with { type: 'json' }
+import docs__about__parentEngagementMdx from './docs__about__parent-engagement.mdx.json' with { type: 'json' }
+import docs__events__indexMdx from './docs__events__index.mdx.json' with { type: 'json' }
+import docs__events__halloweenMdx from './docs__events__halloween.mdx.json' with { type: 'json' }
+import docs__events__holidayFairMdx from './docs__events__holiday-fair.mdx.json' with { type: 'json' }
+import docs__events__earthDayMdx from './docs__events__earth-day.mdx.json' with { type: 'json' }
+import docs__events__piDayMdx from './docs__events__pi-day.mdx.json' with { type: 'json' }
+import docs__events__bakeSaleMdx from './docs__events__bake-sale.mdx.json' with { type: 'json' }
 
-export const allDocs = [docs__inProgressMdx, docs__indexMdx, docs__documentation__codeBlocksMdx, docs__documentation__componentsMdx, docs__documentation__indexMdx, docs__documentation__styleGuideMdx, docs__about__indexMdx]
+export const allDocs = [docs__inProgressMdx, docs__indexMdx, docs__about__committeesMdx, docs__about__electionsMdx, docs__about__executiveBoardMdx, docs__about__faqsMdx, docs__about__indexMdx, docs__about__missionMdx, docs__documentation__bylawsMdx, docs__documentation__externalResourcesMdx, docs__documentation__indexMdx, docs__documentation__styleGuideMdx, docs__documentation__tnsPtaOperationalManualMdx, docs__about__meetings__indexMdx, docs__about__meetings__minutes__2022_23Mdx, docs__about__meetings__minutes__2023_24Mdx, docs__about__meetings__minutes__2024_25Mdx, docs__about__meetings__minutes__indexMdx, docs__about__parentEngagementMdx, docs__events__indexMdx, docs__events__halloweenMdx, docs__events__holidayFairMdx, docs__events__earthDayMdx, docs__events__piDayMdx, docs__events__bakeSaleMdx]

698057f08b39e129f13c634e42661998862baa12
James Washington <jbwashington@gmail.com>
2024-09-12 00:55:51 -0400

    events marquee carousel added to cta

--- config/docs.ts
+++ config/docs.ts
@@ -1,157 +1,160 @@
-import { DocsConfig } from "@/types"
+import { DocsConfig } from "@/types";
 
 export const docsConfig: DocsConfig = {
-  mainNav: [
-    {
-      title: "Documentation",
-      href: "/docs",
-    },
-    {
-      title: "Guides",
-      href: "/guides",
-    },
-  ],
-  sidebarNav: [
-    {
-      title: "Getting Started",
-      items: [
+    mainNav: [
         {
-          title: "Introduction",
-          href: "/docs",
-        },
-      ],
-    },
-    {
-      title: "About",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/about",
+            title: "Documentation",
+            href: "/docs",
         },
         {
-          title: "Executive Board",
-          href: "/docs/about/executive-board",
+            title: "Guides",
+            href: "/guides",
+        },
+    ],
+    sidebarNav: [
+        {
+            title: "Getting Started",
+            items: [
+                {
+                    title: "Introduction",
+                    href: "/docs",
+                },
+            ],
         },
         {
-          title: "Committees",
-          href: "/docs/about/committees",
+            title: "About",
+            items: [
+                {
+                    title: "Introduction",
+                    href: "/docs/about",
+                },
+                {
+                    title: "Executive Board",
+                    href: "/docs/about/executive-board",
+                },
+                {
+                    title: "Committees",
+                    href: "/docs/about/committees",
+                },
+                {
+                    title: "Elections",
+                    href: "/docs/about/elections",
+                },
+                {
+                    title: "Mission",
+                    href: "/docs/about/mission",
+                },
+                {
+                    title: "Parent Engagement",
+                    href: "/docs/about/parent-engagement",
+                },
+                {
+                    title: "FAQs",
+                    href: "/docs/about/faqs",
+                    disabled: true,
+                },
+            ],
         },
         {
-          title: "Elections",
-          href: "/docs/about/elections",
+            title: "Fundraising",
+            items: [
+                {
+                    title: "Introduction",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+                {
+                    title: "Templates",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+                {
+                    title: "Donor Engagement",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+            ],
         },
         {
-          title: "Mission",
-          href: "/docs/about/mission",
+            title: "PTA Meeting Minutes",
+            items: [
+                {
+                    title: "2024-2025",
+                    href: "/docs/about/meetings/minutes/2024-25",
+                    disabled: true,
+                },
+                {
+                    title: "2023-2024",
+                    href: "/docs/about/meetings/minutes/2023-24",
+                    disabled: true,
+                },
+                {
+                    title: "2022-2023",
+                    href: "/docs/about/meetings/minutes/2022-23",
+                    disabled: true,
+                },
+            ],
         },
         {
-          title: "Parent Engagement",
-          href: "/docs/about/parent-engagement",
+            title: "Documentation",
+            items: [
+                {
+                    title: "Introduction",
+                    href: "/docs/in-progress",
+                },
+                {
+                    title: "Bylaws",
+                    href: "/docs/documentation/bylaws",
+                },
+                {
+                    title: "External Resources",
+                    href: "/docs/documentation/external-resources",
+                },
+                {
+                    title: "Style Guide",
+                    href: "/docs/documentation/style-guide",
+                },
+                {
+                    title: "Operational Manual",
+                    href: "/docs/documentation/tns-pta-operational-manual",
+                    disabled: true,
+                },
+            ],
         },
         {
-          title: "FAQs",
-          href: "/docs/about/faqs",
-          disabled: true,
+            title: "Events",
+            items: [
+                {
+                    title: "Introduction",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+                {
+                    title: "Bake Sale",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+                {
+                    title: "Earth Day",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+                {
+                    title: "Halloween",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+                {
+                    title: "Holiday Fair",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+                {
+                    title: "Pi Day",
+                    href: "/docs/in-progress",
+                    disabled: true,
+                },
+            ],
         },
-      ],
-    },
-    {
-      title: "Fundraising",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Templates",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Donor Engagement",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-    {
-      title: "PTA Meeting Minutes",
-      items: [
-        {
-          title: "2024-2025",
-          href: "/docs/about/meetings/minutes/2024-25",
-        },
-        {
-          title: "2023-2024",
-          href: "/docs/about/meetings/minutes/2023-24",
-        },
-        {
-          title: "2022-2023",
-          href: "/docs/about/meetings/minutes/2022-23",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-        },
-        {
-          title: "Bylaws",
-          href: "/docs/documentation/bylaws",
-        },
-        {
-          title: "External Resources",
-          href: "/docs/documentation/external-resources",
-        },
-        {
-          title: "Style Guide",
-          href: "/docs/documentation/style-guide",
-        },
-        {
-          title: "Operational Manual",
-          href: "/docs/documentation/tns-pta-operational-manual",
-          disabled: true,
-        },
-      ],
-    },
-    {
-      title: "Events",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Bake Sale",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Earth Day",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Halloween",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Holiday Fair",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Pi Day",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-  ],
-}
+    ],
+};

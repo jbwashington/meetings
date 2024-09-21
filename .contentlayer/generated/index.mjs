9f87f11eaf4dd24ffaf27f3de0a9f065e9a2f5c5
James Washington <jbwashington@gmail.com>
2024-09-21 17:40:08 -0400

    removed index pages and in progress page

--- .contentlayer/generated/index.mjs
+++ .contentlayer/generated/index.mjs
@@ -4,12 +4,12 @@ export { isType } from 'contentlayer2/client'
 
 // NOTE During development Contentlayer imports from `.mjs` files to improve HMR speeds.
 // During (production) builds Contentlayer it imports from `.json` files to improve build performance.
-import { allPages } from './Page/_index.mjs'
-import { allDocs } from './Doc/_index.mjs'
-import { allGuides } from './Guide/_index.mjs'
-import { allPosts } from './Post/_index.mjs'
-import { allAuthors } from './Author/_index.mjs'
-import { allProducts } from './Product/_index.mjs'
+import allPages from './Page/_index.json' with { type: 'json' }
+import allDocs from './Doc/_index.json' with { type: 'json' }
+import allGuides from './Guide/_index.json' with { type: 'json' }
+import allPosts from './Post/_index.json' with { type: 'json' }
+import allAuthors from './Author/_index.json' with { type: 'json' }
+import allProducts from './Product/_index.json' with { type: 'json' }
 
 export { allPages, allDocs, allGuides, allPosts, allAuthors, allProducts }
 

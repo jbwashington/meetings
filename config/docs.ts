4a6c656f8f6c7e30398f6a7692e37e16065bb8d3
James Washington <jbwashington@gmail.com>
2024-10-09 16:40:27 -0400

    reverted main nav to previous structure and removed links

--- config/docs.ts
+++ config/docs.ts
@@ -3,17 +3,12 @@ import { DocsConfig } from "@/types";
 export const docsConfig: DocsConfig = {
     mainNav: [
         {
-            title: "Links",
-            items: [
-                {
-                    title: "Documentation",
-                    href: "/docs",
-                },
-                {
-                    title: "Guides",
-                    href: "/guides",
-                },
-            ],
+            title: "Docs",
+            href: "/docs",
+        },
+        {
+            title: "Guides",
+            href: "/guides",
         },
     ],
     sidebarNav: [
